210259a4d749e85d5453d8f83f3ecf3ce62cad83
unknown <var1541b@k3-426-6.edu.ugrasu>
2016-01-20 11:34:08 +0500

    Goodbye wold

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
       System.out.print("Hello World!");
+      System.out.print("Goodbye World!");
     }
    
 }
